e61e0388a5f20e4ca5bb6bf267b7106cb2e538db
yiweisi <141038538+yiweisi@users.noreply.github.com>
2026-09-08 08:00:15 +0000

    Deploying to gh-pages from @ yiweisi/yiweisi.github.io@2b2d673b28504f186259623ac08289a938d7fed2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,7 +76,7 @@ ninja.data = [{
           section: "News",},{id: "news-my-dphil-thesis-is-now-available-online-here",
           title: 'My DPhil thesis is now available online here.',
           description: "",
-          section: "News",},{id: "news-new-paper-published-my-article-doch-as-a-hidden-hurdle-to-finiteness-realization-in-german-l1-acquisition-is-now-available-open-access-in-glossa-a-journal-of-general-linguistics",
+          section: "News",},{id: "news-new-paper-published-my-article-doch-as-a-hidden-hurdle-to-finiteness-realization-in-german-l1-acquisition-is-now-available-open-access-in-glossa-a-journal-of-general-linguistics-link",
           title: 'New paper published! My article “Doch as a hidden hurdle to finiteness realization...',
           description: "",
           section: "News",},{
